0df1bb1ae3473d7c101b245de61914f3c10bdace
brayanskad <marquesbrayan4002@gmail.com>
2026-03-25 16:54:36 -0500

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,26 @@
+const lista = document.querySelector(".lista-card");
+
+const vitrine = [
+    {
+        produto: "Teclado mecanico",
+        descricao: "Teclado mecanico com switch blue",
+        preco: "R$ 300,00",
+    },
+    {
+        produto: "Mouse gamer",
+        descricao: "Mouse gamer com 6 botões e iluminação RGB",
+        preco: "R$ 150,00",
+    },
+    {
+        produto: "Monitor 24 polegadas",
+        descricao: "Monitor Full HD com taxa de atualização de 144Hz",
+        preco: "R$ 800,00",
+    },
+    {
+        produto: "Headset gamer",
+        descricao: "Headset com som surround e microfone retrátil",
+        preco: "R$ 200,00",
+    }
+]
+
+c
